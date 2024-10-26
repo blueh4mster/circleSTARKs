f29b8b3afe338266a761177acf8037d1e6a55db9
gerceboss <nilavprajapati2004@gmail.com>
2024-10-26 20:56:59 +0530

    added constants for testing

--- circleStark/src/circle.rs
+++ circleStark/src/circle.rs
@@ -1,3 +1,4 @@
+use lambdaworks_math::elliptic_curve::edwards::curves::bandersnatch::field;
 use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::field::fields::mersenne31::field::Mersenne31Field as M31;
 struct CirclePoint {
@@ -15,5 +16,19 @@ impl CircleImpl for CirclePoint {
             x: self.x.square().double() - FieldElement::one(),
             y: (self.y * self.x).double(),
         };
+    }
+}
+
+pub fn G() -> CirclePoint {
+    CirclePoint {
+        x: 1268011823.into(),
+        y: 2.into(),
+    }
+}
+
+pub fn Z() -> CirclePoint {
+    CirclePoint {
+        x: 1.into(),
+        y: 0.into(),
     }
 }
