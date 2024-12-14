0ac06cae0eb443c41d27271dce56b04a44eae902
gerceboss <nilavprajapati2004@gmail.com>
2024-12-14 20:27:59 +0530

    rearranged

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use lambdaworks_math::field::fields::mersenne31;
 
-mod precomputes;
 mod circle;
-mod utils;
 mod fast_fft;
 mod fast_fri;
-mod fri;
 mod fft;
+mod fri;
 mod line_functions;
 mod merkle;
+mod precomputes;
+mod utils;
 fn main() {
     println!("Hello, world!");
 }
